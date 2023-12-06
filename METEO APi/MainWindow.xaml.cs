21939e046d5768095887028dfc91873d8cc64cbc
yael elkaroui <yael.elkaroui@saintmichelannecy.fr>
2023-12-06 18:35:49 +0100

    VFinal

--- METEO APi/MainWindow.xaml.cs
+++ METEO APi/MainWindow.xaml.cs
@@ -13,8 +13,12 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Net.Http;
+using System.Net;
 using Newtonsoft.Json;
+using System.IO;
+using System.Collections.ObjectModel;
 using System.Reflection.Metadata;
+using static System.Net.WebRequestMethods;
 
 
 namespace METEO_APi
@@ -24,83 +28,248 @@ namespace METEO_APi
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        // Déclarez une liste pour stocker les villes.
+        private List<string> villes = new List<string> { "Annecy" };  // Ajoutez les villes par défaut ici.
         public MainWindow()
         {
+           
             InitializeComponent();
-            _: GetWeather();
-        }
+            City.ItemsSource = villes;
 
-        public async Task<string> GetWeather()
+        _: GetWeather("Annecy");
+
+        }
+        public async Task<string> GetWeather(string city)
         {
             HttpClient client = new HttpClient();
+
             try
             {
-                HttpResponseMessage response = await client.GetAsync("https://www.prevision-meteo.ch/services/json/Annecy");
+                HttpResponseMessage response = await client.GetAsync($"https://www.prevision-meteo.ch/services/json/{city}");
                 if (response.IsSuccessStatusCode)
                 {
                     var content = await response.Content.ReadAsStringAsync();
                     Root root = JsonConvert.DeserializeObject<Root>(content);
-                    CurrentCondition  currentCondition = root.current_condition;
-                    FcstDay0 fcstDay0 = root.fcst_day_0;
-                    string icon = currentCondition.icon_big;
 
-                    //jour 0
-                    TempToday.Text = fcstDay0.day_long.ToString() +": "+ currentCondition.tmp.ToString() + "°C";
-                    meteoNuage.Text = currentCondition.condition.ToString();
-                    HighTemp.Text = "Max: " +fcstDay0.tmax.ToString() + "°C";
-                    LowTemp.Text = "Min: "+fcstDay0.tmin.ToString() + "°C";
-                    Humidité.Text = "Humidité: " + fcstDay0.tmin.ToString() + "%";
-                    
+                    CurrentCondition currentCondition;
 
-                    //jour 1
-                    FcstDay1 fcstDay1 = root.fcst_day_1;
-                    TempToday2.Text = fcstDay1.day_long.ToString()+": " + fcstDay1.tmin.ToString() + "°C";
-                    meteoNuage2.Text = fcstDay1.condition.ToString();
-                    HighTemp2.Text = "Max: " + fcstDay1.tmax.ToString() + "°C";
-                    LowTemp2.Text = "Min: " + fcstDay1.tmin.ToString() + "°C";
-                    Humidité2.Text = "Humidité: " + fcstDay1.tmin.ToString() + "%";
+                    // Vérifie si root et current_condition ne sont pas null
+                    if (root != null && root.current_condition != null)
+                    {
+                        currentCondition = root.current_condition;
+
+                        // Maintenant vous pouvez utiliser currentCondition en toute sécurité
+                        FcstDay0 fcstDay0 = root.fcst_day_0;
+                        TempToday.Text = $"{fcstDay0.day_long}: {currentCondition.tmp}°C";
+                        meteoNuage.Text = currentCondition.condition;
+                        HighTemp.Text = $"Max: {fcstDay0.tmax}°C";
+                        LowTemp.Text = $"Min: {fcstDay0.tmin}°C";
+                        Humidité.Text = $"Humidité: {fcstDay0.tmin}%";
+
+
+                        //jour 1
+                        FcstDay1 fcstDay1 = root.fcst_day_1;
+                        TempToday2.Text = fcstDay1.day_long.ToString()+": " + fcstDay1.tmin.ToString() + "°C";
+                        meteoNuage2.Text = fcstDay1.condition.ToString();
+                        HighTemp2.Text = "Max: " + fcstDay1.tmax.ToString() + "°C";
+                        LowTemp2.Text = "Min: " + fcstDay1.tmin.ToString() + "°C";
+                        Humidité2.Text = "Humidité: " + fcstDay1.tmin.ToString() + "%";
                  
 
-                    //jour 2
-                    FcstDay2 fcstDay2 = root.fcst_day_2;
-                    TempToday3.Text = fcstDay2.day_long.ToString()+ ": " + fcstDay2.tmin.ToString() + "°C";
-                    meteoNuage3.Text = fcstDay2.condition.ToString();
-                    HighTemp3.Text = "Max: " + fcstDay2.tmax.ToString() + "°C";
-                    LowTemp3.Text = "Min: " + fcstDay2.tmin.ToString() + "°C";
-                    Humidité3.Text = "Humidité: " + fcstDay2.tmin.ToString() + "%";
+                        //jour 2
+                        FcstDay2 fcstDay2 = root.fcst_day_2;
+                        TempToday3.Text = fcstDay2.day_long.ToString()+ ": " + fcstDay2.tmin.ToString() + "°C";
+                        meteoNuage3.Text = fcstDay2.condition.ToString();
+                        HighTemp3.Text = "Max: " + fcstDay2.tmax.ToString() + "°C";
+                        LowTemp3.Text = "Min: " + fcstDay2.tmin.ToString() + "°C";
+                        Humidité3.Text = "Humidité: " + fcstDay2.tmin.ToString() + "%";
                   
-                    //jour 3
-                    FcstDay3 fcstDay3 = root.fcst_day_3;
-                    TempToday4.Text = fcstDay3.day_long.ToString()+": " + fcstDay3.tmin.ToString() + "°C";
-                    meteoNuage4.Text = fcstDay3.condition.ToString();
-                    HighTemp4.Text = "Max: " + fcstDay3.tmax.ToString() + "°C";
-                    LowTemp4.Text = "Min: " + fcstDay3.tmin.ToString() + "°C";
-                    Humidité4.Text = "Humidité: " + fcstDay3.tmin.ToString() + "%";
+                        //jour 3
+                        FcstDay3 fcstDay3 = root.fcst_day_3;
+                        TempToday4.Text = fcstDay3.day_long.ToString()+": " + fcstDay3.tmin.ToString() + "°C";
+                        meteoNuage4.Text = fcstDay3.condition.ToString();
+                        HighTemp4.Text = "Max: " + fcstDay3.tmax.ToString() + "°C";
+                        LowTemp4.Text = "Min: " + fcstDay3.tmin.ToString() + "°C";
+                        Humidité4.Text = "Humidité: " + fcstDay3.tmin.ToString() + "%";
                     
-                    //jour 4
-                    FcstDay4 fcstDay4 = root.fcst_day_4;
-                    TempToday5.Text = fcstDay4.day_long.ToString()+": " + fcstDay4.tmin.ToString() + "°C";
-                    meteoNuage5.Text = fcstDay4.condition.ToString();
-                    HighTemp5.Text = "Max: " + fcstDay4.tmax.ToString() + "°C";
-                    LowTemp5.Text = "Min: " + fcstDay4.tmin.ToString() + "°C";
-                    Humidité5.Text = "Humidité: " + fcstDay4.tmin.ToString() + "%";
-                    
+                        //jour 4
+                        FcstDay4 fcstDay4 = root.fcst_day_4;
+                        TempToday5.Text = fcstDay4.day_long.ToString()+": " + fcstDay4.tmin.ToString() + "°C";
+                        meteoNuage5.Text = fcstDay4.condition.ToString();
+                        HighTemp5.Text = "Max: " + fcstDay4.tmax.ToString() + "°C";
+                        LowTemp5.Text = "Min: " + fcstDay4.tmin.ToString() + "°C";
+                        Humidité5.Text = "Humidité: " + fcstDay4.tmin.ToString() + "%";
 
-                    return "";
+                        Icone0.Source = await DownloadImage(fcstDay0.icon_big);
+                        Icone1.Source = await DownloadImage(fcstDay1.icon_big);
+                        Icone2.Source = await DownloadImage(fcstDay2.icon_big);
+                        Icone3.Source = await DownloadImage(fcstDay3.icon_big);
+                        Icone4.Source = await DownloadImage(fcstDay4.icon_big);
 
 
+
+                        return "";
+                    }
+                    else
+                    {
+                        MessageBox.Show("Les données météorologiques pour cette ville ne sont pas disponibles.");
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show($"Erreur de récupération des données météorologiques : {response.ReasonPhrase}");
                 }
 
 
             }
             catch( Exception ex)
             {
-                return "";
+                MessageBox.Show("Erreur lors de la récupération des données météorologiques : " + ex.Message);
             }
 
             return "";
         }
+        private async Task<ImageSource> DownloadImage(string imageUrl)
+        {
+            try
+            {
+                WebClient client = new WebClient();
+                byte[] imageData = await client.DownloadDataTaskAsync(new Uri(imageUrl));
+
+                BitmapImage bitmapImage = new BitmapImage();
+
+                using (MemoryStream stream = new MemoryStream(imageData))
+                {
+                    bitmapImage.BeginInit();
+                    bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                    bitmapImage.StreamSource = stream;
+                    bitmapImage.EndInit();
+                }
+
+                return bitmapImage;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erreur lors du téléchargement de l'image : " + ex.Message);
+                return null;
+            }
+        }
+
+        private async void City_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Vérifie si un élément est sélectionné dans la ComboBox.
+            if (City.SelectedItem != null)
+            {
+                // Récupère la ville sélectionnée.
+                string selectedCity = City.SelectedItem.ToString();
+
+                // Vérifie si la ville sélectionnée existe dans la liste des villes.
+                if (villes.Contains(selectedCity))
+                {
+                    // Appelle la méthode GetMeteo pour obtenir les informations météorologiques pour la ville sélectionnée.
+                    await GetWeather(selectedCity);
+                }
+                else
+                {
+                    MessageBox.Show("Ville non valide. Veuillez sélectionner une ville valide.");
+                }
+            }
+        }
+
+        private async void AddCity_Click(object sender, RoutedEventArgs e)
+        {
+            // Ouvrez une boîte de dialogue pour permettre à l'utilisateur de saisir le nom de la ville.
+            string newCity = Microsoft.VisualBasic.Interaction.InputBox("Enter the name of the city:", "Add New City", "");
+
+            // Vérifiez si l'utilisateur a saisi une ville.
+            if (!string.IsNullOrEmpty(newCity))
+            {
+                // Vérifiez si les coordonnées météo de la ville sont valides.
+                bool isValidCoordinates = await CheckValidCoordinates(newCity);
+
+                if (isValidCoordinates)
+                {
+                    // Ajoutez la nouvelle ville à la liste.
+                    villes.Add(newCity);
+
+                    // Mettez à jour la source de données de la ComboBox.
+                    City.ItemsSource = null;
+                    City.ItemsSource = villes;
+
+                    // Sélectionnez la nouvelle ville ajoutée.
+                    City.SelectedItem = newCity;
+                }
+                else
+                {
+                    MessageBox.Show($"Les coordonnées météo de {newCity} ne sont pas valides. Veuillez entrer une ville valide.");
+                }
+            }
+        }
+
+        private async Task<bool> CheckValidCoordinates(string city)
+        {
+            HttpClient client = new HttpClient();
+
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync($"https://www.prevision-meteo.ch/services/json/{city}");
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    Root root = JsonConvert.DeserializeObject<Root>(content);
+
+                    // Ajoutez une vérification supplémentaire pour les données météo
+                    return root != null && root.current_condition != null;
+                }
+
+                return false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erreur lors de la vérification des coordonnées météo de {city}: {ex.Message}");
+                return false;
+            }
+        }
+
+
+
+        private void RemoveCity_Click(object sender, RoutedEventArgs e)
+        {
+            // Vérifiez s'il y a au moins une ville dans la liste.
+            if (City.Items.Count > 1)
+            {
+                // Obtenez la ville sélectionnée.
+                string selectedCity = City.SelectedItem as string;
+
+                // Supprimez la ville de la liste.
+                villes.Remove(selectedCity);
+
+                // Mettez à jour la source de données de la ComboBox.
+                City.ItemsSource = null;
+                City.ItemsSource = villes;
+
+                // Sélectionnez la première ville après la suppression.
+                City.SelectedIndex = 0;
+
+                // Obtenez la nouvelle ville sélectionnée.
+                string newSelectedCity = City.SelectedItem as string;
+
+                // Appellez la méthode GetWeather pour obtenir les informations météorologiques pour la nouvelle ville sélectionnée.
+                GetWeather(newSelectedCity);
+            }
+            else
+            {
+                MessageBox.Show("Il doit y avoir au moins une ville dans la liste.");
+            }
+        }
+
+       
+
     }
+
+
 
     // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
     public class _0H00
